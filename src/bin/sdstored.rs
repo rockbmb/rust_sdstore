10bb5578cde43536064dcf66bddde8de18af3977
Alexandre Baldé (WSL Win11 Pro) <alexandrer_b@outlook.com>
2023-04-06 18:39:48 +0100

    Used `tempfiles` crate to prepare for unix domain socket binding

--- src/bin/sdstored.rs
+++ src/bin/sdstored.rs
@@ -3,8 +3,9 @@ use rust_sdstore;
 use std::env;
 use std::process;
 
-fn main() {
+use tempfile::Builder;
 
+fn main() {
     rust_sdstore::util::init_logging_infrastructure(
         None, 
         log::LevelFilter::Trace
@@ -15,9 +16,20 @@ fn main() {
     });
 
     let config = rust_sdstore::Config::build(env::args()).unwrap_or_else(|err| {
-        log::warn!("sdstored: Problem parsing arguments: {:?}", err);
+        log::error!("sdstored: Problem parsing arguments: {:?}", err);
         process::exit(1);
     });
+    log::info!("Read config:\n{:?}", config);
 
-    log::info!("Read config:\n{:#?}", config);
+    let tmp_file = Builder::new()
+        .prefix("sdstore")
+        .suffix(".sock")
+        .rand_bytes(0)
+        .tempfile()
+        .unwrap_or_else(|err| {
+            log::error!("sdstored: Problem creating temporary file for unix socket! Error: {:?}", err);
+            process::exit(1);
+        });
+
+    log::info!("filename for unix socket is {:?}", tmp_file);
 }

17ec29fd67938f1768a1e96a04f7c2737f1fff01
Alexandre Baldé (WSL Win11 Pro) <alexandrer_b@outlook.com>
2023-04-06 18:40:17 +0100

    Add stub for client binary

---
+++ src/bin/sdstore.rs
@@ -0,0 +1,12 @@
+use rust_sdstore;
+
+fn main() {
+    rust_sdstore::util::init_logging_infrastructure(
+        None, 
+        log::LevelFilter::Trace
+    ).unwrap_or_else(|err| {
+        eprintln!("Could not init logging infrastructure! Error: {:?}", err);
+        eprintln!("Exiting");
+        std::process::exit(1);
+    });
+}

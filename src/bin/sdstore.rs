3d2761f8a1e03ab06bc6036c72b29f5923f683be
Alexandre Baldé (WSL Win11 Pro) <alexandrer_b@outlook.com>
2023-04-07 03:52:10 +0100

    Use unix domain sockets to read/write data between server/client

--- src/bin/sdstore.rs
+++ src/bin/sdstore.rs
@@ -1,5 +1,9 @@
 use rust_sdstore;
 
+use interprocess::os::unix::udsocket;
+
+use std::{io, process};
+
 fn main() {
     rust_sdstore::util::init_logging_infrastructure(
         None, 
@@ -9,4 +13,35 @@ fn main() {
         eprintln!("Exiting");
         std::process::exit(1);
     });
+
+    let sock_path = format!("tmp/sdstore_{}.sock", process::id());
+
+    let listener = udsocket::UdSocket::bind(sock_path).unwrap_or_else(|err| {
+        log::error!("sdstored: Could not create listener on socket. Error: {:?}", err);
+        process::exit(1);
+    });
+    log::info!("client listening on Unix datagram socket: {:?}", listener);
+    listener.set_destination("./tmp/sdstored.sock").unwrap_or_else(|err| {
+        log::error!("sdstore: error setting client socket destination. Error: {:?}", err);
+        std::process::exit(1);
+    });
+
+    let mut msg = String::new();
+
+    loop {
+        io::stdin()
+            .read_line(&mut msg)
+            .unwrap_or_else(|err| {
+                log::error!("sdstore: could not read from STDIN. Error: {:?}", err);
+                std::process::exit(1);
+            });
+
+        listener.send(msg.as_bytes()).unwrap_or_else(|err| {
+            log::error!("sdstored: Could not send to UdSocket. Error: {:?}", err);
+            process::exit(1);
+        });
+        log::info!("sdstore: wrote {} to UdSocket", msg);
+
+        msg.clear();
+    }
 }
--- src/bin/sdstored.rs
+++ src/bin/sdstored.rs
@@ -1,9 +1,9 @@
-use rust_sdstore;
+use rust_sdstore::*;
 
-use std::env;
-use std::process;
+use interprocess::os::unix::udsocket;
 
-use tempfile::Builder;
+use std::{env, process, str, fs};
+
 
 fn main() {
     rust_sdstore::util::init_logging_infrastructure(
@@ -15,21 +15,34 @@ fn main() {
         std::process::exit(1);
     });
 
-    let config = rust_sdstore::Config::build(env::args()).unwrap_or_else(|err| {
+    let config = config::Config::build(env::args()).unwrap_or_else(|err| {
         log::error!("sdstored: Problem parsing arguments: {:?}", err);
         process::exit(1);
     });
     log::info!("Read config:\n{:?}", config);
 
-    let tmp_file = Builder::new()
-        .prefix("sdstore")
-        .suffix(".sock")
-        .rand_bytes(0)
-        .tempfile()
-        .unwrap_or_else(|err| {
-            log::error!("sdstored: Problem creating temporary file for unix socket! Error: {:?}", err);
+    let sock_path = create_file_for_udsock("tmp/sdstored.sock").unwrap_or_else(|err| {
+        log::error!("sdstored: Could not create file for UdSocket. Error {:?}", err);
+        process::exit(1);
+    });
+    log::info!("server's socket path is: {:?}", sock_path);
+
+    fs::remove_file(sock_path.clone()).unwrap();
+    let listener = udsocket::UdSocket::bind(sock_path).unwrap_or_else(|err| {
+        log::error!("sdstored: Could not create listener on socket. Error: {:?}", err);
+        process::exit(1);
+    });
+    log::info!("server listening on Unix datagram socket: {:?}", listener);
+
+    let mut buf = [0; 1024];
+
+    loop {
+        let (n, _) = listener.recv(&mut buf).unwrap_or_else(|err| {
+            log::error!("sdstored: Could not read from UdSocket. Error: {:?}", err);
             process::exit(1);
         });
+        let msg = &buf[..n];
 
-    log::info!("filename for unix socket is {:?}", tmp_file);
+        log::info!("sdstored: read {}\n from UdSocket", str::from_utf8(msg).unwrap());
+    }
 }
